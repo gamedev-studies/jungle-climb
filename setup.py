a721eff377c017e7c08971c74e244d3697e749b2
Elijah Lopez <elijahllopezz@gmail.com>
2019-12-12 02:08:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ import sys
 NAME = 'Jungle Climb'
 VERSION = '1.5'
 PACKAGES = ['pygame']
-installed_packages = reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze']).decode('utf-8')
+installed_packages = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze']).decode('utf-8')
 installed_packages = installed_packages.split('\r\n')
-EXCLUDES = [pkg.split('==')[0] for pkg in installed_packages if pkg != '']
-print(EXCLUDES)
+EXCLUDES = {pkg.split('==')[0] for pkg in installed_packages if pkg != ''}
+EXCLUDES.add('tkinter')
 for pkg in PACKAGES:
     EXCLUDES.remove(pkg)
 
 
 executables = [Executable('main.py', base='Win32GUI', icon='Resources/Jungle Climb Icon.ico', targetName=NAME)]
-
 setup(
     name=NAME,
     version=VERSION,
