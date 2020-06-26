57091ad2d1c2eb7f60fcb44853664c0d6e107378
elibroftw <xxelibroxx@gmail.com>
2020-06-26 12:19:09 -0400

    Better build script

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 
 NAME = 'Jungle Climb'
+SETUP_OUTPUT_NAME = 'Jungle Climb Setup'
 PACKAGES = ['pygame']
 installed_packages = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze']).decode('utf-8')
 installed_packages = installed_packages.split('\r\n')
@@ -13,6 +14,18 @@ EXCLUDES = {pkg.split('==')[0] for pkg in installed_packages if pkg != ''}
 EXCLUDES.add('tkinter')
 for pkg in PACKAGES:
     EXCLUDES.remove(pkg)
+
+with open('setup_script.iss', 'r+') as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        if line.startswith('#define MyAppVersion'):
+            lines[i] = f'#define MyAppVersion "{VERSION}"\n'
+        elif line.startswith('OutputBaseFilename'):
+            lines[i] = f'OutputBaseFilename={SETUP_OUTPUT_NAME}\n'
+            break
+    f.seek(0)
+    f.writelines(lines)
+    f.truncate()
 
 copyright_text = f'Copyright 2019 - {datetime.now().year} Elijah Lopez'
 executables = [Executable('main.py', base='Win32GUI', icon='resources/Jungle Climb Icon.ico', targetName=NAME)]
