7fbf1c75aa2d668aed04031b41a4a135ad3f22c0
Gabriel U <ullmanng162@gmail.com>
2022-05-09 14:26:44 -0400

    add training debug script

--- climber_ai_train_debug.py
+++ climber_ai_train_debug.py
@@ -5,12 +5,12 @@ env = ClimberAgent()
 results = []
 model = PPO('MlpPolicy', env, verbose=1, tensorboard_log='tensorboard')
 obs = env.reset()
-model.save("models/model1")
+model.load("models/model1")
 for i in range(0, 20):
     obs = env.reset()
     for episode in range(50000):
-        action, _state = model.predict(obs, deterministic=True)
-        obs, reward, done, info = env.step(action)
+        action, _state = model.predict(obs, deterministic=False)
+        obs, reward, done, info = env.step(-1)
         env.render()
         if done:
             print("no more lives")
